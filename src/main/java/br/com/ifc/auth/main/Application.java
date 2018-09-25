cb362ddf785eb60067dc98590bf6118bda7e29d2
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-25 11:49:32 -0300

    ---

--- src/main/java/br/com/ifc/auth/main/Application.java
+++ src/main/java/br/com/ifc/auth/main/Application.java
@@ -2,10 +2,12 @@ package br.com.ifc.auth.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableMongoRepositories(basePackages={"br.com.ifc.auth.repository"})
 @SpringBootApplication(scanBasePackages= {"br.com.ifc.auth.config", "br.com.ifc.auth.controller", "br.com.ifc.auth.filter", "br.com.ifc.auth.service"})
 public class Application {
